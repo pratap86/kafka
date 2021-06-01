d6f035e2a7fb30561ba584c3fc6c4c9cd530b254
pratap86 <narayanpratap86@gmail.com>
2021-06-01 21:31:56 +0530

    initial project setup only

---
+++ library-events-consumer/src/test/java/com/pratap/kafka/LibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratap.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
